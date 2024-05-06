d84bc7629614ca6bec7bdcdfceb7f4ba79837cd2
Yuri_Gomes <gomesyuridasilva@gmail.com>
2024-05-06 15:37:54 -0300

    fix: exercício 5

--- ex5.js
+++ ex5.js
@@ -1,9 +1,9 @@
 let n1 = prompt("Digite o primeiro numero:")
-nota1 = eval (n1)
+
 let n2 = prompt("Digite o segundo numero:")
-nota2 = eval (n2)
+
 let n3 = prompt("Digite o terceiro numero:")
-nota3 = eval (n3)
+
 
 if(n1 < n2 && n2 < n3){
     console.log("Os numeros estão em ordem crescente.")
