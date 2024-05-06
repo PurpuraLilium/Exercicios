41eabd570607d653b61ad941679b75696d550b21
Yuri_Gomes <gomesyuridasilva@gmail.com>
2024-05-06 14:21:37 -0300

    feat: exercício 3

---
+++ ex3.js
@@ -0,0 +1,10 @@
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = prompt("Digite um numero")
+
+if (numero %2 == 0){
+    console.log("Seu numero é par.")
+}
+else {
+    console.log("seu numero é impar")
+}
