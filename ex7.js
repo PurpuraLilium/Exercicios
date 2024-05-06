39359dcfb14f2bcf9a2f04e50bcd9689c394384e
Yuri_Gomes <gomesyuridasilva@gmail.com>
2024-05-06 16:29:17 -0300

    feat: exercício 7

--- ex7.js
+++ ex7.js
@@ -6,10 +6,13 @@ vinte = price * 0.2
 
 if (price > 100.0 && price <= 200.0){
     price = price - dez
+
+    console.log("Seu desconto foi de ", dez)
 }
 
 else if (price > 200.0){
     price = price - vinte
+    console.log("Seu desconto foi de ", vinte)
 }
 
 console.log("O valor final da sua compra é de: R$", price)
